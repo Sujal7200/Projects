7b670da1bb0de77c0b166e74e0165cf6ccb2220f
Sujal7200 <sujalm7200@gmail.com>
2024-11-12 15:39:10 -0600

    License Requirements Added

--- Tour Scheduling Software/tourSchedule.py
+++ Tour Scheduling Software/tourSchedule.py
@@ -1,5 +1,20 @@
-
-
+"""
+ * Personal License
+ * 
+ * Author: Sujal More
+ * Project: AutomatedTourScheduling Software
+ * 
+ * This code is provided solely for educational and
+ * personal use. Unauthorized copying, distribution,
+ * or commercial use of this code, in whole or in
+ * part, without the explicit permission of the author
+ * is strictly prohibited.
+ * 
+ * For permissions or inquiries, please contact:
+ * sujalm7002@gmail.com
+ * 
+ * © 2024 Sujal More. All rights reserved.
+"""
 
 
 import pandas as pd
